8e8e70936e479bf122583da2b0eb87997f78dcd6
heon <siwmua0@naver.com>
2022-12-31 16:42:14 +0900

    [YoutubeCloneAPP] Feat: 코드 리뷰하고 마무리

--- YoutubeClone-APP/youtube_clone-app/src/components/Card.jsx
+++ YoutubeClone-APP/youtube_clone-app/src/components/Card.jsx
@@ -1,15 +1,19 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { useNavigate } from "react-router-dom";
 import { timeAgo } from "util/data";
 
-const Card = ({ list }) => {
+const Card = ({ list, type }) => {
   const { channelTitle, publishedAt, thumbnails, title } = list.snippet;
 
   const navigate = useNavigate();
+  const isList = type === "list";
 
   return (
-    <div onClick={() => navigate(`/video/detail/${list.id}`, { state: { list } })}>
-      <img className="w-full" src={thumbnails.medium.url} alt="" />
+    <div
+      className={isList ? "flex gap-1 m-2" : ""}
+      onClick={() => navigate(`/video/detail/${list.id}`, { state: { list } })}
+    >
+      <img className={isList ? "w-60 mr-2" : "w-full"} src={thumbnails.medium.url} alt="" />
       <div>
         <div className="font-semibold text-base line-clamp-2 ">{title}</div>
         <div>
--- YoutubeClone-APP/youtube_clone-app/src/components/Detail.jsx
+++ YoutubeClone-APP/youtube_clone-app/src/components/Detail.jsx
@@ -1,5 +1,5 @@
 import Reference from "components/Reference";
-import React, { useState } from "react";
+import React from "react";
 import { useLocation, useParams } from "react-router-dom";
 import Info from "./Info";
 
@@ -9,23 +9,26 @@ const Detail = () => {
   const { title, channelId, channelTitle, description } = state.list.snippet;
 
   return (
-    <div className=" ">
-      <div className="">
+    <div className="flex flex-col lg:flex-row">
+      <div className="basis-4/6">
         <div>
           <iframe
             width="100%"
             height="640"
             src={`https://www.youtube.com/embed/${id}`}
             title="YouTube video player"
+            frameborder="0"
           ></iframe>
         </div>
-        <div className="w-[560px]">
-          <div>{title}</div>
+        <div className="p-8">
+          <h2 className="text-xl font-bold">{title}</h2>
           <Info id={channelId} name={channelTitle} />
-          <pre>{description}</pre>
+          <pre className="whitespace-pre-wrap">{description}</pre>
         </div>
       </div>
-      <Reference id={id} />
+      <div className="basis-2/6">
+        <Reference id={id} />
+      </div>
     </div>
   );
 };
